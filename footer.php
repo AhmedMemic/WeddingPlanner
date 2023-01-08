1a4a314ee9935be01d85456a9d2ad204f95cf068
Ahmed <a.memic@signundsinn.de>
2023-01-08 23:29:41 +0100

    initial commit

---
+++ footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>Categories</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+            <a href="portfolio.php"><i class="fas fa-angle-right"></i> Portfolio</a>
+            <a href="pricing.php"><i class="fas fa-angle-right"></i> Pricing</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i> Contact</a>
+        </div>
+
+        <div class="box">
+            <h3>Info</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> Plan Wedding</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Our Services</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Ask Question</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Terms of Use</a>
+            <a href="#"><i class="fas fa-angle-right"></i> Privacy Policy</a>
+        </div>
+
+        <div class="box">
+            <h3>Imprint</h3>
+            <a href="#"><i class="fas fa-phone"> +123-456-7890</i></a>
+            <a href="#"><i class="fas fa-envelope"> wedding-planner@gmail.com</i></a>
+            <a href="#"><i class="fas fa-map"> Sarajevo 7100, Bosnia and Herzegovina</i></a>
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook-f"> Facebook</i></a>
+            <a href="#"><i class="fab fa-twitter"> Twitter</i></a>
+            <a href="#"><i class="fab fa-instagram"> Instagram</i></a>
+            <a href="#"><i class="fab fa-linkedin"> Linkedin</i></a>
+            <a href="#"><i class="fab fa-github"> GitHub</i></a>
+        </div>
+    </div>
+
+    <p class="credit"> Created by <span>Web Designer</span> | All rights reserved!</p>
+</section>
